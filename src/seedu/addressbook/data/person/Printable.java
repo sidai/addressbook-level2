a71d2c1ab10f3837ac6a3cdc983116790232910f
chen sidai <c13413968687@gmail.com>
2016-09-07 23:24:59 +0800

    Adds a Printable interface and overrides the getPrintableString in classes Name, Phone, Email, and Address

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+/**
+ * A printable immutable interface for a Person in the addressbook.
+ */
+
+public interface Printable {
+	
+	public String getPrintableString();
+	
+}
